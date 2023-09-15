bb5dd6276cb537f7dd10b5087731b7b5cfbba9bf
Ilya Shumikhin <ilya.shumikhin@rt.ru>
2023-09-15 23:31:34 +0300

    Выполнена травка муравка

--- bin/surf_dart_courses_template.dart
+++ bin/surf_dart_courses_template.dart
@@ -1,1 +1,24 @@
-void main(List<String> arguments) {}
+import 'input.dart';
+
+void main(List<String> arguments) {
+  List<Territory> territories = mapAfter2010.values
+      .expand((element) => element)
+      .toList()
+    ..addAll(mapBefore2010.values.expand((element) => element).toList());
+  List<num> machinesAges = territories
+      .expand((e) => e.machineries)
+      .toSet()
+      .map((e) => DateTime.now().year - e.releaseDate.year)
+      .toList()
+    ..sort((a, b) => b.compareTo(a));
+
+  print("Средний возраст всей техники ${averageFrom(machinesAges)}");
+
+  final middleIndex = (machinesAges.length / 2).ceil();
+  print(
+      "Средний возраст 50% старой техники ${averageFrom(machinesAges.sublist(0, middleIndex))}");
+}
+
+num averageFrom(List<num> list) {
+  return list.fold<num>(0, (a, b) => a + b) / list.length;
+}
